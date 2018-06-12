5258dbaccd742063aac8fd50224acb1ba931b968
naveenperera777 <naveenperera777@gmail.com>
2018-06-12 23:52:39 +0530

    Registration and Login

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,9 +13,9 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
-        //
-    }
+{
+    Schema::defaultStringLength(191);
+}
 
     /**
      * Register any application services.
